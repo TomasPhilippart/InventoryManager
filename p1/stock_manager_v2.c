d9fa02dcc51eb4517b47310671e421b6764f17f9
Tomás <tomasphilippart@gmail.com>
2020-03-27 18:39:39 +0000

    Update

--- p1/stock_manager_v2.c
+++ p1/stock_manager_v2.c
@@ -8,7 +8,7 @@
 
 /* Retirar comentario para ativar funcao 
    de debug, executada pelo carater 'D' */
-/* #define DEBUG */
+#define DEBUG
 
 #define MAX_DESCRICAO 64
 #define MAX_PRODUTOS 10000
@@ -16,7 +16,7 @@
 #define MAX_PESO 200
 
 /* estes 2 defines sao usados para os parametros da 
-   matriz 2x2 conj_produtos, na estrutura Encomenda */
+   matriz 2x2 produtos, na estrutura Encomenda */
 #define IDP 0
 #define QTD 1
 
@@ -29,36 +29,41 @@ typedef struct Produto {
        > Descricao (int descricao) - max 63 carateres
        > Preco (int preco) - (> 0)
        > Peso (int peso) - (> 0)
-       > Quantidade em stock (int qtd) - (>= 0) */
+       > Quantidade em stock (int stock) - (>= 0) */
 
-    int preco, peso, qtd;
+    int preco, peso, stock;
     char descricao[MAX_DESCRICAO];
-}Produto;
-
-/* Tabela com capacidade para armazenar todos os produtos */
-Produto produtos[MAX_PRODUTOS];
-
-/* Variavel global para manter em conta o numero de produtos */
-int idp_max = 0;
+} Produto;
 
 
 /* ============================ Encomenda ============================*/
 /* Uma encomenda e um conjunto de produtos */
 typedef struct Encomenda {
 
-   /* Array conj_produtos que guarda os produtos na encomenda 
-      na forma [[idp_0, preco_0], [idp_1, preco_1], ...] */
-   int conj_produtos[MAX_PRODUTOS][2];
+   /* Array produtos que guarda os produtos na encomenda 
+      na forma [[idp_0, qtd_0], [idp_1, qtd_1], ...] */
+   int produtos[MAX_PRODUTOS][2];
 
    int peso, num_produtos_distintos;
-}Encomenda;
+} Encomenda;
 
 
-/* Tabela com capacidade para armazenar todas as encomendas */
-Encomenda encomendas[MAX_ENCOMENDAS];
+/* ============================ Armazem ============================*/
+/* O armazem contem o conjunto de produtos e o conjunto de encomendas */
 
-/* Variavel global para manter em conta o numero de encomendas */
-int ide_max = 0;
+struct Armazem {
+
+    /* Tabela com capacidade para armazenar todos os produtos */
+    Produto produtos[MAX_PRODUTOS];
+    /* Variavel para manter em conta o numero de produtos */
+    int num_produtos;
+
+    /* Tabela com capacidade para armazenar todas as encomendas */
+    Encomenda encomendas[MAX_ENCOMENDAS];
+    /* Variavel para manter em conta o numero de encomendas */
+    int num_encomendas;    
+} armazem = {0};
+
 
 /* Prototipo de funcoes */
 #ifdef DEBUG
@@ -68,8 +73,10 @@ void novo_produto();
 void add_stock();
 void nova_encomenda();
 void add_prod_encomenda();
+void novo_prod_encomenda(int ide, int idp, int qtd);
 void remove_stock();
 void remove_prod_encomenda();
+void remove_produto_encomenda_aux(int ide, int idp);
 void calcula_custo_encomenda();
 void set_preco_prod();
 void lista_descricao_qtd_prod_encomenda();
@@ -155,13 +162,14 @@ int main() {
 
 /* Adicona um novo produto ao sistema. */
 void novo_produto() {
-    scanf(" %[^:]:%d:%d:%d", produtos[idp_max].descricao, &produtos[idp_max].preco, 
-                             &produtos[idp_max].peso, &produtos[idp_max].qtd);
 
-    printf("Novo produto %d.\n", idp_max);
-    idp_max++; 
+    int idp = armazem.num_produtos;
+    scanf(" %[^:]:%d:%d:%d", armazem.produtos[idp].descricao, &armazem.produtos[idp].preco,
+                             &armazem.produtos[idp].peso, &armazem.produtos[idp].stock);
+
+    printf("Novo produto %d.\n", idp);
+    armazem.num_produtos++; 
 }
-
 
 /* Adiciona stock a um produto existente no sistema. */
 void add_stock() {
@@ -169,21 +177,23 @@ void add_stock() {
     int idp, qtd;
     scanf(" %d:%d", &idp, &qtd);  
 
-    if (idp >= idp_max) {
+    if (idp >= armazem.num_produtos) {
         printf("Impossivel adicionar produto %d ao stock. Produto inexistente.\n", idp);
     }
-    produtos[idp].qtd += qtd;
+    armazem.produtos[idp].stock += qtd;
 }
 
 
 /* Cria uma nova encomenda. */
 void nova_encomenda() {
 
-    encomendas[ide_max].peso = 0;
-    encomendas[ide_max].num_produtos_distintos = 0;
+    int ide = armazem.num_encomendas;
 
-    printf("Nova encomenda %d.\n", ide_max);
-    ide_max++;
+    armazem.encomendas[ide].peso = 0;
+    armazem.encomendas[ide].num_produtos_distintos = 0;
+
+    printf("Nova encomenda %d.\n", armazem.num_encomendas);
+    armazem.num_encomendas++;
 }
 
 
@@ -191,66 +201,71 @@ void nova_encomenda() {
    ja existir na encomenda, adiciona quantidade. */
 void add_prod_encomenda() {
 
-   int ide, idp, qtd, i, num_produtos;
+   int ide, idp, qtd, i;
 
    scanf(" %d:%d:%d", &ide, &idp, &qtd);
 
     /* Verificar se a encomenda existe */
-    if (ide >= ide_max) {
+    if (ide >= armazem.num_encomendas) {
         printf("Impossivel adicionar produto %d a encomenda %d. Encomenda inexistente.\n", idp, ide);
         return;
     }
 
     /* Verificar se o produto existe */
-    if (idp >= idp_max) {
+    if (idp >= armazem.num_produtos) {
         printf("Impossivel adicionar produto %d a encomenda %d. Produto inexistente.\n", idp, ide);
         return;
     }
 
     /* Verificar se a quantidade em stock e suficiente */
-    if (qtd > produtos[idp].qtd) {
+    if (qtd > armazem.produtos[idp].stock) {
         printf("Impossivel adicionar produto %d a encomenda %d. Quantidade em stock insuficiente.\n", idp, ide);
         return;
     }
 
     /* Verificar se o peso da encomenda excede o maximo de 200 */
-    if (encomendas[ide].peso + (produtos[idp].peso * qtd) > MAX_PESO) {
+    if (armazem.encomendas[ide].peso + (armazem.produtos[idp].peso * qtd) > MAX_PESO) {
         printf("Impossivel adicionar produto %d a encomenda %d. Peso da encomenda excede o maximo de 200.\n", idp, ide);
         return;
     }
 
     /* procurar pelo produto na encomenda */
-    for (i = 0; i < encomendas[ide].num_produtos_distintos; i++) {
+    for (i = 0; i < armazem.encomendas[ide].num_produtos_distintos; i++) {
 
         /* se o produto ja existir na encomenda, adiciona quantidade */
-        if (encomendas[ide].conj_produtos[i][IDP] == idp) {
-            encomendas[ide].conj_produtos[i][QTD] += qtd;
+        if (armazem.encomendas[ide].produtos[i][IDP] == idp) {
+            armazem.encomendas[ide].produtos[i][QTD] += qtd;
 
             /* subtrai a quantidade do stock */
-            produtos[idp].qtd -= qtd;
+            armazem.produtos[idp].stock -= qtd;
 
             /* aumenta peso*/ 
-            encomendas[ide].peso += produtos[idp].peso * qtd;
+            armazem.encomendas[ide].peso += armazem.produtos[idp].peso * qtd;
             return;
         } 
     }
 
+    novo_prod_encomenda(ide, idp,qtd);
+}
+
+
+/* Funcao auxiliar que CRIA um produto e ADICIONA-o a uma encomenda */ 
+void novo_prod_encomenda(int ide, int idp, int qtd) {
     /* variavel auxiliar, para nao repetir o comboio */
-    num_produtos = encomendas[ide].num_produtos_distintos;
+    int num_produtos = armazem.encomendas[ide].num_produtos_distintos;
 
     /* adiciona o produto a encomenda, substituindo a quantidade e o identificador */
-    encomendas[ide].conj_produtos[num_produtos][QTD] = qtd;
-    encomendas[ide].conj_produtos[num_produtos][IDP] = idp;
+    armazem.encomendas[ide].produtos[num_produtos][QTD] = qtd;
+    armazem.encomendas[ide].produtos[num_produtos][IDP] = idp;
     
     /* aumenta peso*/ 
-    encomendas[ide].peso += produtos[idp].peso * qtd;
+    armazem.encomendas[ide].peso += armazem.produtos[idp].peso * qtd;
 
     /* incrementar o numero de produtos distintos*/
-    encomendas[ide].num_produtos_distintos++;
+    armazem.encomendas[ide].num_produtos_distintos++;
 
     /* subtrai a quantidade do stock */
-    produtos[idp].qtd -= qtd;
-
+    armazem.produtos[idp].stock -= qtd;
 }
 
 
@@ -260,80 +275,87 @@ void remove_stock() {
     scanf(" %d:%d", &idp, &qtd_a_remover);
 
     /* Verificar se o produto existe */
-    if (idp >= idp_max) {
+    if (idp >= armazem.num_produtos) {
         printf("Impossivel remover stock do produto %d. Produto inexistente.\n", idp);
         return;
     }
 
     /* Verificar se a quantidade em stock e suficiente */
-    if (qtd_a_remover > produtos[idp].qtd) {
+    if (qtd_a_remover > armazem.produtos[idp].stock) {
         printf("Impossivel remover stock do produto %d. Quantidade insuficiente.\n", idp);
         return;
     }
 
-    produtos[idp].qtd -= qtd_a_remover;
+    armazem.produtos[idp].stock -= qtd_a_remover;
 }
 
 
 /* Remove um produto de uma encomenda */
 void remove_prod_encomenda() {
-    int ide, idp, i, j;
+    int ide, idp, i;
 
 
     scanf(" %d:%d", &ide, &idp);
 
     /* Verificar se a encomenda existe */
-    if (ide >= ide_max) {
+    if (ide >= armazem.num_encomendas) {
         printf("Impossivel remover produto %d a encomenda %d. Encomenda inexistente.\n", idp, ide);
         return;
     }
 
     /* Verificar se o produto existe */
-    if (idp >= idp_max) {
+    if (idp >= armazem.num_produtos) {
         printf("Impossivel remover produto %d a encomenda %d. Produto inexistente.\n", idp, ide);
         return;
     }
 
 
     /* adiciona stock ao produto a remover */
-    produtos[idp].qtd += encomendas[ide].conj_produtos[idp][QTD];
+    armazem.produtos[idp].stock += armazem.encomendas[ide].produtos[idp][QTD];
 
     /* remove peso da encomenda */
-    encomendas[ide].peso -= produtos[idp].peso * 
-                            encomendas[ide].conj_produtos[idp][QTD];
+    armazem.encomendas[ide].peso -= armazem.produtos[idp].peso * 
+                            armazem.encomendas[ide].produtos[idp][QTD];
 
     /* percorrer o tabela ate encontrar o produto a remover */
-    for (i = 0; i < encomendas[ide].num_produtos_distintos; i++) {
-        if (encomendas[ide].conj_produtos[i][IDP] == idp) {
-            /* encontrado o produto a remover, vou passar os produtos "depois" dele 
-               para a frente */
-            for (j = idp; j < encomendas[ide].num_produtos_distintos; j++) {
-                /* "empurrar" os produtos para tras, substituindo o produto a remover */
-                encomendas[ide].conj_produtos[j][IDP] = encomendas[ide].conj_produtos[j + 1][IDP];
-                encomendas[ide].conj_produtos[j][QTD] = encomendas[ide].conj_produtos[j + 1][QTD];
-            }
+    for (i = 0; i < armazem.encomendas[ide].num_produtos_distintos; i++) {
+        if (armazem.encomendas[ide].produtos[i][IDP] == idp) {
+            remove_produto_encomenda_aux(ide, idp);
 
-            encomendas[ide].num_produtos_distintos--;
+            armazem.encomendas[ide].num_produtos_distintos--;
             return;
         }
     }
 }
 
+/* Funcao auxiliar para "remover" o produto de uma encomenda*/
+void remove_produto_encomenda_aux(int ide, int idp) {
+    int j;
+    /* encontrado o produto a remover, vou passar os produtos "depois" dele 
+        para a "esquerda" */
+    for (j = idp; j < armazem.encomendas[ide].num_produtos_distintos; j++) {
+        /* "empurrar" os produtos para tras, substituindo o produto a remover */     
+            armazem.encomendas[ide].produtos[j][IDP] = armazem.encomendas[ide].produtos[j + 1][IDP];
+            armazem.encomendas[ide].produtos[j][QTD] = armazem.encomendas[ide].produtos[j + 1][QTD];
+            }
+}
 
 /* Calcula custo da encomenda */
 void calcula_custo_encomenda() {
-    int i, ide, custo = 0;
+    int i, qtd, ide, idp, custo = 0;
 
     scanf(" %d", &ide);
 
     /* Verificar se a encomenda existe */ 
-    if (ide >= ide_max) {
+    if (ide >= armazem.num_encomendas) {
         printf("Impossivel calcular custo da encomenda %d. Encomenda inexistente.\n", ide);
         return;
     }
 
-    for (i = 0; i < encomendas[ide].num_produtos_distintos; i++) {
-        custo += (produtos[encomendas[ide].conj_produtos[i][IDP]].preco * encomendas[ide].conj_produtos[i][QTD]);
+    for (i = 0; i < armazem.encomendas[ide].num_produtos_distintos; i++) {
+        idp = armazem.encomendas[ide].produtos[i][IDP];
+        qtd = armazem.encomendas[ide].produtos[i][QTD];
+        custo += (armazem.produtos[idp].preco * qtd);
     }
 
     printf("Custo da encomenda %d %d\n", ide, custo);
@@ -347,12 +369,12 @@ void set_preco_prod() {
     scanf(" %d:%d", &idp, &preco);
 
     /* Verificar se o produto existe */
-    if (idp >= idp_max) {
+    if (idp >= armazem.num_produtos) {
         printf("Impossivel alterar preco do produto %d. Produto inexistente.", idp);
         return;
     }
 
-    produtos[idp].preco = preco;
+    armazem.produtos[idp].preco = preco;
 }
 
 
@@ -363,22 +385,22 @@ void lista_descricao_qtd_prod_encomenda() {
     scanf(" %d:%d", &ide, &idp);
 
     /* Verificar se a encomenda existe */ 
-    if (ide >= ide_max) {
+    if (ide >= armazem.num_encomendas) {
         printf("Impossivel listar encomenda %d. Encomenda inexistente.\n", ide);
         return;
     }
 
     /* Verificar se o produto existe */
-    if (idp >= idp_max) {
+    if (idp >= armazem.num_produtos) {
         printf("Impossivel listar produto %d. Produto inexistente.", idp);
         return;
     }
 
-    printf("%s %d", produtos[idp].descricao, encomendas[ide].conj_produtos[idp][QTD]);
+    printf("%s %d", armazem.produtos[idp].descricao, armazem.encomendas[ide].produtos[idp][QTD]);
 }
 
 
-/* lista o identificaidor da encomenda em que o produto dado ocorre mais vezes */
+/* lista o identificador da encomenda em que o produto dado ocorre mais vezes */
 void lista_ide_max_produto() {
 
     int i, j, idp, ide_max_produto, max_quantidade = 0;
@@ -386,27 +408,28 @@ void lista_ide_max_produto() {
     scanf(" %d", &idp);
 
     /* Verificar se o produto existe */
-    if (idp >= idp_max) {
+    if (idp >= armazem.num_produtos) {
         printf("Impossivel listar maximo do produto %d. Produto inexistente.\n", idp);
         return;
     }
     /* iteracao por todas as encomendas */
-    for (j = 0; j < ide_max; j++) {
+    for (j = 0; j < armazem.num_encomendas; j++) {
 
         /* iteracao por todos os produtos da encomenda j */
-        for (i = 0; i < encomendas[j].num_produtos_distintos; i++) {
+        for (i = 0; i < armazem.encomendas[j].num_produtos_distintos; i++) {
             /* se encontrarmos o produto numa dada encomenda*/
-            if (encomendas[j].conj_produtos[i][IDP] == idp) {
+            if (armazem.encomendas[j].produtos[i][IDP] == idp) {
                 /* se a quantidade for maior do que a quantidade do maior produto,
                 esta sera a encomenda onded o produto ocorre mais vezes */
-                if (encomendas[j].conj_produtos[i][QTD] > max_quantidade) {
+                if (armazem.encomendas[j].produtos[i][QTD] > max_quantidade) {
                     ide_max_produto = j;
-                    max_quantidade = encomendas[i].conj_produtos[i][QTD];
+                    max_quantidade = armazem.encomendas[i].produtos[i][QTD];
                 }
             }
         }   
 
     }
+
     /* se nao houver encomendas */
     if (max_quantidade == 0) return;
     
@@ -416,12 +439,38 @@ void lista_ide_max_produto() {
 
 
 void lista_produtos_por_preco() {
-
+    
 }
 
 void lista_produtos_alfabeticamente_descricao() {
     
 }
+
+int particao(Produto p[], int lo, int hi) {
+    int i, pivot = p[lo].preco, left = lo;
+
+    for (i = lo + 1; i <= hi; i++) {
+        if (p[i].preco < pivot) {
+            troca(p[i], p[left]);
+            left++;
+        }
+    }
+
+    troca(p[lo], p[left]);
+
+    return left;
+}
+
+void quicksort(Produto p[], int lo, int hi) {
+    int index;
+    if (lo < hi) {
+        index = particao(p, lo, hi);
+        quicksort(p, lo, index);
+        quicksort(p, index + 1, hi);
+    }
+}
+
+
 
 /* Funcao de debug (chamada pelo comando 'D') */
 #ifdef DEBUG
@@ -429,20 +478,23 @@ void lista_produtos_alfabeticamente_descricao() {
         int i,j;
 
         /* iterar pelas encomendas */
-        for(i = 0; i < ide_max ; i++){
+        for(i = 0; i < armazem.num_encomendas ; i++){
 
             printf("Encomenda %d\n",i);
 
             /* iterar pelos produtos da encomenda */
-            for(j = 0; j < encomendas[i].num_produtos_distintos; j++){
-                printf("\tProduto:%d Descricao:%s Quantidade:%d\n", encomendas[i].conj_produtos[j][IDP], 
-                                                                    produtos[encomendas[i].conj_produtos[j][IDP]].descricao, 
-                                                                    encomendas[i].conj_produtos[j][QTD]);
+            for(j = 0; j < armazem.encomendas[i].num_produtos_distintos; j++){
+                printf("\tProduto:%d Descricao:%s Quantidade:%d\n", armazem.encomendas[i].produtos[j][IDP], 
+                                                                    armazem.produtos[armazem.encomendas[i].produtos[j][IDP]].descricao, 
+                                                                    armazem.encomendas[i].produtos[j][QTD]);
             }
 
-            printf("\tPeso:%d\n",encomendas[i].peso);
+            printf("\tPeso:%d\n",armazem.encomendas[i].peso);
 
             putchar('\n');
         }
     }
 #endif
+
+
+
